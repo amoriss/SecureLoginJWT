3e499adf8954ecb83b4d44aa00ee3c19e824b6af
Amoriss <amoriss@protonmail.com>
2024-11-06 16:45:42 -0600

    completed GenerateJwtToken method

--- SecureLoginJWT/Controllers/AccountController.cs
+++ SecureLoginJWT/Controllers/AccountController.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using SecureLoginJWT.Models;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace SecureLoginJWT.Controllers;
 public class AccountController : Controller
@@ -6,5 +11,47 @@ public class AccountController : Controller
     public IActionResult Index()
     {
         return View();
+    }
+
+    // GET
+    public ActionResult Login()
+    {
+        return View();
+    }
+    // POST
+    [HttpPost]
+    public ActionResult Login(LoginModel model)
+    {
+        if (model.Username == "admin" && model.Password == "password") 
+        {
+            var token = GenerateJwtToken(model.Username);
+
+            Response.Cookies.Append("auth_token", token, new CookieOptions
+            {
+                HttpOnly = true,  // cookie is not accessible via JavaScript (added security)
+                Secure = true,  // cookie is only sent over HTTPS
+                SameSite = SameSiteMode.Strict  // protection against Cross-Site Forgery
+            });
+
+            return RedirectToAction("Index", "Home");
+        }
+        return View(model);
+    }
+
+    // Generate JWT Token
+    private string GenerateJwtToken(string username)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes("TopSecretKey");
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new System.Security.Claims.ClaimsIdentity(new[] { new Claim(ClaimTypes.Name, username) }), // add claims (username) to the token
+            Expires = DateTime.UtcNow.AddHours(1), // token's expiration time
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature) // signs the token using secret key
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+        return tokenHandler.WriteToken(token);
     }
 }
--- SecureLoginJWT/Controllers/HomeController.cs
+++ SecureLoginJWT/Controllers/HomeController.cs
@@ -27,4 +27,5 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+
 }
