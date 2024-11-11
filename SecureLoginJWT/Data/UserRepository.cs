af78a74c63062364c5d02afc7d581c9a9523312d
Amoriss <amoriss@protonmail.com>
2024-11-10 23:55:37 -0600

    added GetUserByUsernameAsync method

--- SecureLoginJWT/Data/UserRepository.cs
+++ SecureLoginJWT/Data/UserRepository.cs
@@ -26,4 +26,28 @@ public class UserRepository
         var result = await command.ExecuteNonQueryAsync();
         return result > 0;
     }
+
+    public async Task<UserCredentials?> GetUserByUsernameAsync(string username)
+    {
+        using var connection = new MySqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        var query = "SELECT id, username, email, password_hash FROM user_credentials WHERE username =@Username";
+
+        using var command = new MySqlCommand(query, connection);
+        command.Parameters.AddWithValue("@Username", username);
+
+        using var reader = await command.ExecuteReaderAsync();
+        if(await reader.ReadAsync())
+        {
+            return new UserCredentials
+            {
+                Id = reader.GetInt32("id"),
+                Username = reader.GetString("username"),
+                Email = reader.GetString("email"),
+                PasswordHash = reader.GetString("password")
+            };
+        }
+        return null;
+    }
 }
